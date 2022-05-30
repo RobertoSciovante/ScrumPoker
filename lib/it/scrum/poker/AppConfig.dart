4370be4f4a747ff6b52a7f25c00177e95307b0c3
rober <robertosciovante@startingfinance.com>
2022-05-30 12:14:50 +0200

    workflow_dispatch

--- lib/it/scrum/poker/AppConfig.dart
+++ lib/it/scrum/poker/AppConfig.dart
@@ -4,29 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
-class Flavor {
-  Flavor._internal(this.name, this.color) {
-    // values.add(this);
-  }
-
-  static final Flavor DEV = Flavor._internal('dev', Colors.amber);
-  static final Flavor TEST = Flavor._internal('test', Colors.blue);
-  static final Flavor PROD = Flavor._internal('prod', Colors.teal);
-  static List<Flavor> values = [DEV, TEST, PROD];
-
-
-  static Flavor? valueOf(String name) {
-    for (var element in values) {
-      if (element.name == name) {
-        return element;
-      }
-    }
-    return null;
-  }
-
-  final String name;
-  final Color color;
-}
+// class Flavor {
+//   Flavor._internal(this.name, this.color) {
+//     // values.add(this);
+//   }
+//
+//   static final Flavor DEV = Flavor._internal('dev', Colors.amber);
+//   static final Flavor TEST = Flavor._internal('test', Colors.blue);
+//   static final Flavor PROD = Flavor._internal('prod', Colors.teal);
+//   static List<Flavor> values = [DEV, TEST, PROD];
+//
+//
+//   static Flavor? valueOf(String name) {
+//     for (var element in values) {
+//       if (element.name == name) {
+//         return element;
+//       }
+//     }
+//     return null;
+//   }
+//
+//   final String name;
+//   final Color color;
+// }
 
 class AppConfig {
   // factory AppConfig() {
@@ -40,17 +40,16 @@ class AppConfig {
   //
   // static final _AppConfig _singleton = _AppConfig._internal();
 
-  static  late Flavor _flavor;
-  static Flavor get flavor => _flavor;
+  // static  late Flavor _flavor;
+  // static Flavor get flavor => _flavor;
 
-  static  bool get debugMode => kDebugMode;
+  static bool get debugMode => kDebugMode;
 
   // static late SharedPreferences _preferences;
 
-  static   Future<void> init() async {
-    // _preferences = await SharedPreferences.getInstance();
-    _flavor = Flavor.valueOf(const String.fromEnvironment('flavor')) ?? Flavor.PROD;
-
+  static Future<void> init() async {
+    //   _preferences = await SharedPreferences.getInstance();
+    // _flavor = Flavor.valueOf(const String.fromEnvironment('flavor')) ?? Flavor.PROD;
+    //
   }
-
 }
